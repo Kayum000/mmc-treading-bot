77ecef6f7f82cd4c4da5693b96ad0ac93debbd3e
Md. A. Kayum <106771481+Kayum000@users.noreply.github.com>
2026-09-10 23:25:15 +0600

    Fix WebView chooser imports

--- android/app/src/main/java/com/kayum/mmc_trading_bot/MainActivity.java
+++ android/app/src/main/java/com/kayum/mmc_trading_bot/MainActivity.java
@@ -17,6 +17,8 @@ import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
+import android.os.Handler;
+import android.os.Looper;
 import android.provider.Settings;
 import android.webkit.JavascriptInterface;
 import android.webkit.ValueCallback;
